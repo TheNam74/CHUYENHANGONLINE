5821c7925606c4c3477dac368a3bd186abfffd9e
Tung Nguyen <ncnt257@gmail.com>
2021-12-16 22:34:22 +0700

    add actors list pages

---
+++ CHUYENHANGONLINE/Admin/CustomerListWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CHUYENHANGONLINE.Admin
+{
+    /// <summary>
+    /// Interaction logic for CustomerListWindow.xaml
+    /// </summary>
+    public partial class CustomerListWindow : Window
+    {
+        private BindingList<Customer.Customer> _customerList;
+        public CustomerListWindow(BindingList<Customer.Customer> customerList)
+        {
+            _customerList = customerList;
+            InitializeComponent();
+        }
+        private void CustomerListWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            CustomerListView.Items.Clear();
+            CustomerListView.ItemsSource = _customerList;
+        }
+    }
+}
--- CHUYENHANGONLINE/Customer/Customer.cs
+++ CHUYENHANGONLINE/Customer/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CHUYENHANGONLINE.Customer
 {
-    class Customer:IUser
+    public class Customer:IUser
     {
         public int Id { get; set; }
         public int LoginId { get; set; }
